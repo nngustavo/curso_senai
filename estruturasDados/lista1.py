eb4c75f6628dc9aa1c0f2e6fe97bc0809a653694
nngustavo <vw.gustavo@gmail.com>
2023-11-21 20:07:11 -0300

    update lista 1

--- estruturasDados/lista1.py
+++ estruturasDados/lista1.py
@@ -6,4 +6,18 @@ nomes = ["Luan", "Gustavo", "Ester", "Iago"]
 
 for nome in nomes:
     print(nome)
-    
+
+#procurar um nome na lista
+if "Vinicius" in nomes:
+    print("Aluno está presente!")
+
+else:
+    print("Aluno faltou!")
+
+#Concatenar outra lista
+
+outros_nomes = ["Helder", "Aduyar"]
+
+todos_alunos = nomes + outros_nomes
+
+print(todos_alunos)
