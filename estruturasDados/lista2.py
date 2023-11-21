a8596727e0c3613ae4fbedb7604ba18d879f955a
nngustavo <vw.gustavo@gmail.com>
2023-11-21 20:13:53 -0300

    lista 2

---
+++ estruturasDados/lista2.py
@@ -0,0 +1,20 @@
+numeros = [5,2,8,1,9]
+
+print(numeros)
+
+#ordenar os numeros
+
+numeros.sort()
+print(numeros)
+
+soma = sum(numeros)
+
+print(soma)
+
+#maximo e minimo
+
+minimo = min (numeros)
+maximo = max (numeros)
+
+print(minimo)
+print(maximo)
